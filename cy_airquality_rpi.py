ab11067b4fc9909052ba6f496cdae094abe34fbb
Sinnie <sinnieonfire@gmail.com>
2023-06-18 21:55:20 +0300

    Auto refresh every 10 minutes

--- cy_airquality_rpi.py
+++ cy_airquality_rpi.py
@@ -4,7 +4,7 @@ import time
 import Adafruit_CharLCD as LCD
 from unidecode import unidecode
 
-# Raspberry Pi pin configuration
+# Raspberry Pi pin and LCD configuration
 lcd_rs        = 25
 lcd_en        = 24
 lcd_d4        = 23
@@ -21,33 +21,45 @@ lcd_rows    = 2
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
                            lcd_columns, lcd_rows, lcd_backlight)
 
-# Send a GET request to the webpage
-url = "https://www.airquality.dli.mlsi.gov.cy/"
-response = requests.get(url)
+while True:
+    # Update screen
+    lcd.clear()
 
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+    # Send a GET request to the webpage
+    url = "https://www.airquality.dli.mlsi.gov.cy/"
+    response = requests.get(url)
 
-# Find the div containing the data
-divs = soup.find_all("div", class_="col col-xs-12 col-sm-6 col-md-3 col-lg-2 col-3")
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
 
-# Loop through the divs and extract the data for Limassol only
-for div in divs:
-    # Extract the location, pollutant, and value data
-    location_element = div.find("h4")
-    pollutant_label = div.find("span", class_="pollutant-label")
-    pollutant_value = div.find("span", class_="pollutant-value")
+    # Find the div containing the data where class is the div with info needed
+    divs = soup.find_all("div", class_="col col-xs-12 col-sm-6 col-md-3 col-lg-2 col-3")
+
+    # Loop through the divs and extract the data for Limassol only
+    for div in divs:
+        # Extract the location, pollutant, and value data
+        location_element = div.find("h4")
+        pollutant_label = div.find("span", class_="pollutant-label")
+        pollutant_value = div.find("span", class_="pollutant-value")
+        
+        # Check if elements are found
+        if location_element and pollutant_label and pollutant_value and "Limassol" in location_element.text:
+            location = location_element.text.strip()
+            pollutant = pollutant_label.text.strip()
+            value = pollutant_value.text.strip()
+            # Print result
+            print(f"Pollutant: {pollutant}, Value: {value}")
+            
+#        else:
+#           print("PARSE\nERROR")
+#           lcd.message("PARSE\nERROR")
+# Todo: error handling
+
+    # Normalize unicode text since LCD can't render unicode symbols
+    unicode_text = (f"Pollutant {pollutant}\nValue {value}")
+    normalized_text = unidecode(unicode_text)
     
-    # Check if elements are found
-    if location_element and pollutant_label and pollutant_value and "Limassol" in location_element.text:
-        location = location_element.text.strip()
-        pollutant = pollutant_label.text.strip()
-        value = pollutant_value.text.strip()
-        # Print or process the extracted data as needed
-        print(f"Pollutant: {pollutant}, Value: {value}")
-
-# Normalize unicode text since LCD can't render unicode symbols
-unicode_text = (f"Pollutant {pollutant}\nValue {value}")
-normalized_text = unidecode(unicode_text)
-
-lcd.message(normalized_text)
+    # Render on screen
+    lcd.message(normalized_text)
+    # Update every 10 minutes
+    time.sleep(600)
