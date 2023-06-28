1183072711de0fd54547fe2dc3e54a91436dd4ec
Sinnie <sinnieonfire@gmail.com>
2023-06-28 19:45:47 +0300

    [2] Rendering scrapped data as formatted lines with scrolling animation

--- cy_airquality_rpi.py
+++ cy_airquality_rpi.py
@@ -5,21 +5,20 @@ import Adafruit_CharLCD as LCD
 from unidecode import unidecode
 
 # Raspberry Pi GPIO pin and LCD use configuration
-lcd_rs        = 25
-lcd_en        = 24
-lcd_d4        = 23
-lcd_d5        = 17
-lcd_d6        = 18
-lcd_d7        = 22
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 18
+lcd_d7 = 22
 lcd_backlight = 4
 lcd_columns = 16
-lcd_rows=2
+lcd_rows = 2
 
 lcd_columns = 16
-lcd_rows    = 2
+lcd_rows = 2
 
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-                           lcd_columns, lcd_rows, lcd_backlight)
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 while True:
     # Refresh screen
@@ -40,32 +39,43 @@ while True:
         h4_header = div_element.find("h4", class_="field-content stations-overview-title")
 
         if h4_header:
-            # Get the station name
+            # Get the station city
             station_name = h4_header.text.strip()
 
             # Find all pollutant labels and values within the div
             pollutant_labels = div_element.find_all(class_="pollutant-label")
             pollutant_values = div_element.find_all(class_="pollutant-value")
-        
-            # Extract and print the pollutant labels and values
-            print(f"Station: {station_name}")
+
+            # Create a list of formatted pollutant lines
+            pollutant_lines = []
             for label, value in zip(pollutant_labels, pollutant_values):
                 pollutant_label = label.text.strip().rstrip(":")
                 pollutant_value = value.text.strip()
                 if pollutant_value == "Not Measured":
-                        pollutant_value = "N/A"
+                    pollutant_value = "N/A"
                 print(f"Pollutant {pollutant_label}, value: {pollutant_value}")
+                line = f"Pollutant {pollutant_label}\nValue {pollutant_value}"
+                pollutant_lines.append(line)
+
+                # Scroll through the pollutant lines on the LCD display
+                for line in pollutant_lines:
+                # Clear the LCD screen
+                    lcd.clear()
+
+                # Normalize unicode text since LCD can't render unicode symbols
+                normalized_text = unidecode(line)
+
+                # Scroll the line on the LCD display
+                for i in range(len(normalized_text) - lcd_columns + 1):
+                    lcd.message(normalized_text[i:i+lcd_columns])
+                    time.sleep(0.5)  # Adjust the scrolling speed here
+
+                # Pause at the end of each line
+                time.sleep(10)  # Adjust the pause duration here
         else:
             print("Could not find table for Limassol, webpage updated or unavailable")
     else:
         print("Could not find the table, webpage updated or unavailable")
 
-    # Normalize unicode text since LCD can't render unicode symbols
-    unicode_text = (f"Pollutant {pollutant_label}\nValue {pollutant_value}")
-    normalized_text = unidecode(unicode_text)
-    
-    # Render on screen
-    lcd.message(normalized_text)
-    
     # Update every 10 minutes
     time.sleep(600)
